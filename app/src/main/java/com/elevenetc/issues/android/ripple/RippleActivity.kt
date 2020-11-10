39d98f33352186b071b4a16f84cce7758b812da7
elevenetc <shumvlesu@gmail.com>
2020-11-10 14:40:57 +0100

    Add ripple activity

---
+++ app/src/main/java/com/elevenetc/issues/android/ripple/RippleActivity.kt
@@ -0,0 +1,12 @@
+package com.elevenetc.issues.android.ripple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elevenetc.issues.android.R
+
+class RippleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ripple2)
+    }
+}
